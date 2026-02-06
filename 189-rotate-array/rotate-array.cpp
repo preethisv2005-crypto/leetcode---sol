456d5e28e612e1c2e577fb0e14331859c311a220
Preethi S V <preethisv2005@gmail.com>
2026-02-06 23:34:06 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (68.05%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <algorithm> // for std::reverse
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n; // Handle cases where k is greater than n
+        reverse(nums.begin(), nums.end()); // Reverse the entire array
+        reverse(nums.begin(), nums.begin() + k); // Reverse the first k elements
+        reverse(nums.begin() + k, nums.end()); // Reverse the rest of the elements
+    }
+};
