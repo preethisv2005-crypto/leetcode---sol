dad9dda8935f98cadf0e601d742334a7f091058b
Preethi S V <preethisv2005@gmail.com>
2026-02-24 22:12:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (80.26%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        if (matrix.empty()) return {};
-        
         int m = matrix.size();
         int n = matrix[0].size();
-        
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-        vector<int> ans;
-        
-        int dr[] = {0, 1, 0, -1};
-        int dc[] = {1, 0, -1, 0};
-        
-        int r = 0, c = 0, di = 0; 
-        
-        for (int i = 0; i < m * n; ++i) {
-            ans.push_back(matrix[r][c]);
-            visited[r][c] = true;
-            
-            int next_r = r + dr[di];
-            int next_c = c + dc[di];
-            
-           
-            if (next_r >= 0 && next_r < m && next_c >= 0 && next_c < n && !visited[next_r][next_c]) {
-                r = next_r;
-                c = next_c;
-            } else {
-                di = (di + 1) % 4; 
-                r += dr[di];
-                c += dc[di];
+        int left = 0 ;
+        int top = 0 ;
+        int right = n-1 ;
+        int bottom = m-1 ;
+        vector<int>ans ;
+        while (top <= bottom && left <= right){
+            for(int i = left ; i<= right ; i++){
+                ans.push_back(matrix[top][i]);
             }
+            top++ ;
+            for(int i = top ; i<= bottom ; i++){
+                ans.push_back(matrix[i][right]);
+            }
+            right-- ;
+            if(top <= bottom){
+                for(int i = right ; i>= left ; i--){
+                ans.push_back(matrix[bottom][i]);
+                }
+                bottom -- ;
+            }
+            if(left <= right){
+                for(int i = bottom ; i>= top ; i--){
+                ans.push_back(matrix[i][left]);
+                }
+                left++ ;
+            }
+
         }
+        return ans ;
         
-        return ans;
     }
 };
